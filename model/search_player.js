beac194417943cb570c68ec1c5765031cee1c470
mdshahsamir <mdshahsamir@gmail.com>
2021-05-03 00:24:46 +0600

    search_player.js work going

--- model/search_player.js
+++ model/search_player.js
@@ -1,1 +1,15 @@
-//search player
+var db_conn = require('./db_connection');
+var con = db_conn.getConnection();
+
+exports.searchPlayer = function (req,res) {
+    const game = req.query.game;
+    const page = req.query.page;
+   
+
+    var sql = "SELECT * FROM user where email=?";
+    con.query(sql, [email], function (err, result) {
+        console.log(result[0]);
+        res.json(result[0]);
+    })
+
+}
