179cf83991ab68dbdd7809bb341a64444b546b68
mdshahsamir <mdshahsamir@gmail.com>
2022-07-22 23:52:09 +0600

    db config changed

--- model/db_connection.js
+++ model/db_connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "gamingbuddyapi"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "bdcb0964024fe7",
+    password: "b7fb9b26",
+    database: "heroku_a8eee1adfb448c3"
 });
 
 connection.connect(function (err) {
