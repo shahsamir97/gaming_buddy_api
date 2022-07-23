7b03723ec27ece3d76f524bf50f20457c0f872ac
mdshahsamir <mdshahsamir@gmail.com>
2022-07-23 14:10:51 +0600

    db_conn file  changed

--- model/db_connection.js
+++ model/db_connection.js
@@ -10,20 +10,21 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) {
         console.log("Database disconnected");
-    }
+    }else
+        console.log("Database connected");
 });
 
 connection.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
+          console.log("PROTOCOL_CONNECTION_LOST")                  // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
         throw err;                                  // server variable configures this)
     }
 });
 
 
-handleDisconnect();
+
 
 exports.getConnection = function () {
     return connection
