713ae4a1386883ea431fe6d87d6647ef17418ea3
mdshahsamir <mdshahsamir@gmail.com>
2022-07-23 14:07:18 +0600

    db con chaged

--- model/db_connection.js
+++ model/db_connection.js
@@ -1,21 +1,30 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "bdcb0964024fe7",
-    password: "b7fb9b26",
-    database: "heroku_a8eee1adfb448c3"
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "b6ed446cb9886e",
+    password: "3c83becd",
+    database: "heroku_ce45d63fb4417ef"
 });
 
 connection.connect(function (err) {
     if (err) {
         console.log("Database disconnected");
     }
-    console.log("Database connected");
 });
+
+connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+    }
+});
+
+
+handleDisconnect();
 
 exports.getConnection = function () {
     return connection
 }
-
-
